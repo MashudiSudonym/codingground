64bfc5759976efa686d4fbb3923808f69dff04fd
MashudiSudonym <muhamadmashudiardiwinata@gmail.com>
2017-03-05 08:47:53 +0700

    Created by Coding Ground Client

--- Pretest Pemahaman Java/Tugas8/BiodataTgs8.java
+++ Pretest Pemahaman Java/Tugas8/BiodataTgs8.java
@@ -1,16 +1,9 @@
-class Biodata{
+public class BiodataTgs8{
 	String nama = "Muhamad Mashudi";
 	String alamat = "Jepara";
 	int umur = 21;
 
 	void CekBiodata(){
 		System.out.println("Nama : " + nama + "\nAlamat : " + alamat + "\nUmur : " + umur);
-	}
-}
-
-public class Tugas8{
-	public static void main(String[] args){
-		Biodata b = new Biodata();
-		b.CekBiodata();
 	}
 }
